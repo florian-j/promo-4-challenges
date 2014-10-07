f86e9080c5eb9cd65544cb452e9f09a3248f2243
florian-j <florian.joulie@gmail.com>
2014-10-07 16:03:21 +0200

    test rake

--- 01-Ruby/01-Programming-basics/05-Stupid-coaching/lib/coach_answer.rb
+++ 01-Ruby/01-Programming-basics/05-Stupid-coaching/lib/coach_answer.rb
@@ -1,8 +1,12 @@
 def coach_answer(your_message)
   # TODO: return coach answer to your_message
+  if your_message.split("").last == "?"
+  puts "Silly question, get dressed and go to work!"
+  else
+  puts "I don't care, get dressed and go to work!"
+  end
 end
 
 def coach_answer_enhanced(your_message)
   # TODO: return coach answer to your_message, with additional custom rules of yours!
 end
-
--- 01-Ruby/01-Programming-basics/05-Stupid-coaching/lib/interface.rb
+++ 01-Ruby/01-Programming-basics/05-Stupid-coaching/lib/interface.rb
@@ -1,2 +1,9 @@
 require_relative "coach_answer"
 # TODO: Implement the program that makes you discuss with your coach from the terminal.
+puts "Message à votre coach"
+your_message = gets.chomp
+
+until your_message == "I am going to work right now!"
+  coach_answer(your_message)
+  your_message = gets.chomp
+end
