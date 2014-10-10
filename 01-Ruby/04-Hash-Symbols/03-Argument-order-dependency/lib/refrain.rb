7e0bd6b4f849965ba71b83a98eedf0358e580dc0
florian-j <florian.joulie@gmail.com>
2014-10-10 17:09:52 +0200

    exo

--- 01-Ruby/04-Hash-Symbols/03-Argument-order-dependency/lib/refrain.rb
+++ 01-Ruby/04-Hash-Symbols/03-Argument-order-dependency/lib/refrain.rb
@@ -2,14 +2,31 @@ def refrain(lyrics, number_of_times = 1, vibrato = 0, strong = false)
   refrain = []
   lyrics += lyrics[lyrics.size-1] * vibrato
   lyrics.upcase! if strong
-  
-  number_of_times.times do 
+
+  number_of_times.times do
     refrain << lyrics
   end
-  
+
   refrain.join(" ")
 end
 
-def better_refrain(lyrics, options = {vibrato: 0, number_of_times: 1})
+#p refrain("au clair de la lune", 3, 5)
+
   #TODO: implement this better version which breaks argument order dependency
+
+def better_refrain(lyrics, options = {})
+refrain = []
+  lyrics += lyrics[lyrics.size-1] * options[:vibrato].to_i
+  lyrics.upcase! if options[:strong]
+
+  options[:number_of_times].times do
+    refrain << lyrics
+  end
+refrain.join(" ")
 end
+
+
+p better_refrain("au clair de la lune", {vibrato: 3, number_of_times: 4, strong: true})
+
+
+
