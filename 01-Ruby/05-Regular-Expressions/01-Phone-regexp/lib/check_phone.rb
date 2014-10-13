dc42480e43b266b3845f277c9617ea4a6626612d
florian-j <florian.joulie@gmail.com>
2014-10-13 15:35:53 +0200

    exo

--- 01-Ruby/05-Regular-Expressions/01-Phone-regexp/lib/check_phone.rb
+++ 01-Ruby/05-Regular-Expressions/01-Phone-regexp/lib/check_phone.rb
@@ -1,3 +1,13 @@
 def french_phone_number?(phone_number)
   # TODO: true or false?
+  # commence par 0 et contient 10 digits
+  # ou commence par +33 et contient 11 digits
+
+  if phone_number =~ /^(0|\+33)[\s-]?[167][\s-]?(\d{2}[\s-]?){4}$/
+    return true
+  else
+    return false
+  end
 end
+
+p french_phone_number?("06 06 06 06 06")
