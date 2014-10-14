a63877fcf3c9b25d495a4bf2cce787dfa5fb70b1
florian-j <florian.joulie@gmail.com>
2014-10-14 12:36:32 +0200

    exo

--- 01-Ruby/05-Regular-Expressions/02-Anagrams/lib/anagrams.rb
+++ 01-Ruby/05-Regular-Expressions/02-Anagrams/lib/anagrams.rb
@@ -1,7 +1,22 @@
 def anagrams?(a_string, another_string)
   # TODO: implement the obvious method to test if two words are anagrams
+  a_string_sort = a_string.split.sort
+  another_string_sort = another_string.split.sort
+  if a_string_sort == another_string_sort
+    true
+  else
+    false
+  end
 end
 
-def anagrams_on_steroids?(a_string, another_string)
-  # TODO: implement the improved method
-end
+
+p anagrams?("abc", "cab")
+
+
+
+
+
+
+# def anagrams_on_steroids?(a_string, another_string)
+#   # TODO: implement the improved method
+# end
--- 01-Ruby/06-Parsing/01-CSV-Parsing/lib/box_office.rb
+++ 01-Ruby/06-Parsing/01-CSV-Parsing/lib/box_office.rb
@@ -1,6 +1,24 @@
-# Encoding: utf-8
+﻿# Encoding: utf-8
 require 'csv'
 
 def most_successfull(number, max_year, file_name)
   # TODO: return the number most successfull movies max_year
+
+  csv_options = { col_sep: ',' }
+
+  tab_movies = []
+
+  CSV.foreach(file_name, csv_options) do |row|
+    hash_movie = {
+      name: row[0],
+      year: row[1].to_i,
+      earnings: row[2].to_i
+    }
+    tab_movies << hash_movie
+  end
+
+  tab_movies.select! { |movie| movie[:year] < max_year }
+  tab_movies.sort { |movie| movie[:earnings] }
+  tab_movies.take(number)
 end
+
