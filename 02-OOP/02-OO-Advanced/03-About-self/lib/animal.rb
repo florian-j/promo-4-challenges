c4590b9cd18e84c3e2405b61af5247745c270a16
florian-j <florian.joulie@gmail.com>
2014-10-16 16:55:05 +0200

    exo

--- 02-OOP/02-OO-Advanced/03-About-self/lib/animal.rb
+++ 02-OOP/02-OO-Advanced/03-About-self/lib/animal.rb
@@ -1,25 +1,37 @@
 class Animal
+  attr_reader :name, :specie, :color, :natural_habitat
+
   def name(name)
     @name = name
     # TODO: find good instruction
+    self
   end
 
   def specie(specie)
     @specie = specie
     # TODO: find good instruction
+    self
   end
 
   def color(color)
     @color = color
     # TODO: find good instruction
+    self
   end
 
   def natural_habitat(natural_habitat)
     @natural_habitat = natural_habitat
     # TODO: find good instruction
+    self
   end
 
   def to_s
     "Name: #{@name}, Specie: #{@specie}, Color: #{@color}, Natural Habitat: #{@natural_habitat}"
   end
 end
+
+# animal1 = Animal.new().name("bob").specie("fox").color("red").natural_habitat("forrest")
+# p animal1.to_s
+
+p Animal.new.name("Fox")
+p Animal.new.to_s
