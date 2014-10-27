f627408792a5f895832fe185fc97ed193f06a108
florian-j <florian.joulie@gmail.com>
2014-10-27 11:40:25 +0100

    exo

--- 03-AR-Database/03-ActiveRecord-Basics/01-Migrations/db/migrate/20141025152200_create_posts.rb
+++ 03-AR-Database/03-ActiveRecord-Basics/01-Migrations/db/migrate/20141025152200_create_posts.rb
@@ -1,5 +1,18 @@
 class CreatePosts < ActiveRecord::Migration
   def change
     # TODO: your code here to create the posts table
+    create_table :posts do |t|
+      t.string    :name
+      t.string    :url
+      t.timestamps  # add 2 columns, `created_at` and `updated_at`
+    end
+  end
+end
+
+
+
+class AddColumn < ActiveRecord::Migration
+  def change
+    add_column :posts, :votes, :integer
   end
 end
