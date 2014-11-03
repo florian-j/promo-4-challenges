fd2be7b7766326ef607fa4977b8eeacf00bacc86
florian-j <florian.joulie@gmail.com>
2014-11-03 14:50:31 +0100

    circles

--- 04-Front-End/04-Javascript-jQuery/02-Events-Animation/application.js
+++ 04-Front-End/04-Javascript-jQuery/02-Events-Animation/application.js
@@ -1,5 +1,13 @@
-/*
-  *
-  * TODO: add your code here!
-  *
-*/
+$(function() {
+
+  $('#message').delay(1000).slideDown('slow');
+  $('#message').on('click', function(e) {
+    $(this).hide();
+  $('#message-wrapper').on('mouseenter', function(e) {
+    $('#message').show()
+    });
+  $('#message-wrapper').on('mouseleave', function(e) {
+    $('#message').hide()
+    });
+  });
+});
